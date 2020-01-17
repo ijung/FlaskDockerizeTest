2422b259f1af7597e4d2bca42a3885b9e775108d
정인준 <ijung@ebay.com>
2020-01-17 13:47:22 +0900

    Flask Dockerize Test

--- flask_web/app.py
+++ flask_web/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Team Study!'
+    return 'Hello, Team Study! »≠¿Ã∆√!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
